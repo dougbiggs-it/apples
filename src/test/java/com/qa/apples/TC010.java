f3d84d7ea6b39b9e3ff6daf741668eccbbe8797a
doug <dougbiggs.it@gmail.com>
2020-01-13 16:46:23 -0500

    first commit

---
+++ src/test/java/com/qa/apples/TC010.java
@@ -0,0 +1,13 @@
+package com.qa.apples;
+
+import org.testng.annotations.Test;
+
+public class TC010 {
+
+	@Test
+	public void CopyOrderAndComplete()
+	{
+		System.out.println("CopyOrderAndComplete... TC010");
+	}
+	
+}
